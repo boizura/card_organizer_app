386d48e3523156e63e3515b412e1364b2f314272
Maliyah Fleming <liyahfleming6404@gmail.com>
2026-03-05 13:07:46 -0500

    updated the folder and card repo

--- lib/repositories/card_repository.dart
+++ lib/repositories/card_repository.dart
@@ -1,4 +1,6 @@
 import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import '../database_helper.dart';
 import '../models/card.dart';
 
